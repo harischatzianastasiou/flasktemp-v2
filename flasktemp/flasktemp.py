213f72c8277c875ad3ef14f7716b72161ebcfd50
tchatzian <75744098+tchatzian@users.noreply.github.com>
2023-10-13 01:52:37 +0300

    Update flasktemp.py

--- flasktemp/flasktemp.py
+++ flasktemp/flasktemp.py
@@ -13,76 +13,49 @@ app = Flask(__name__)
 def main():
     # Enter your API key here
     api_key = "03c73a62f90e9a82746f188332b5c8eb"
-
     # base_url variable to store url
     base_url = "http://api.openweathermap.org/data/2.5/weather?"
-
     # Give city name
     #city_name = input("Enter city name : ")  
     city_name = args.city_name
     # complete_url variable to store
     # complete url address
     complete_url = base_url + "appid=" + api_key + "&q=" + city_name
-
     # get method of requests module
     # return response object
     response = requests.get(complete_url)
-
     # json method of response object 
     # convert json format data into
     # python format data
     x = response.json()
-
     # Now x contains list of nested dictionaries
     # Check the value of "cod" key is equal to
     # "404", means city is found otherwise,
     # city is not found
     if x["cod"] != "404":
-
          # store the value of "main"
          # key in variable y
          y = x["main"]
-
          # store the value corresponding
          # to the "temp" key of y
          current_temperature = y["temp"]
          #Converts from kelvin to celcius
          current_temperature=current_temperature - 273
-
-         # store the value corresponding
-         # to the "pressure" key of y
-         current_pressure = y["pressure"]
-
-         # store the value corresponding
-         # to the "humidity" key of y
-         current_humidiy = y["humidity"]
-          
          # store the value of "weather"
          # key in variable z
          z = x["weather"]
-
          # store the value corresponding 
          # to the "description" key at 
          # the 0th index of z
          weather_description = z[0]["description"]
-
          # print following values
          return """
          <meta http-equiv="refresh" content={} />
          Temperature (in celcius)<br><br>: {}.""".format(args.refresh,current_temperature)
-          #"\n atmospheric pressure (in hPa unit) = " +
-          #          str(current_pressure) +
-          #"\n humidity (in percentage) = " +
-          #          str(current_humidiy) +
-          #"\n description = " +
-          #          str(weather_description))
-          #
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("server", help="specify address and port")
-    parser.add_argument("refresh", help="specify refresh rate",
-                        type=int)
     parser.add_argument("city_name", help="specify city name for openweatherapi")
     args = parser.parse_args()
     app.config.update (
